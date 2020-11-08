2393aaad454a44d7195d7c4dee27f2108c7513ac
luunhutung <>
2020-11-08 10:17:16 +0700

    Day 08 - 8/11/2020

---
+++ Day08WebApplication1/Day08WebApplication1/Controllers/DemoController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Day08WebApplication1.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.Web.CodeGeneration.Contracts.Messaging;
+
+namespace Day08WebApplication1.Controllers
+{
+    public class DemoController : Controller
+    {
+        #region Demo Async
+        public string DemoSync()
+        {
+            var demo = new Demo();
+            var stopWatch = new Stopwatch();
+
+            stopWatch.Start();
+
+            demo.Test01();
+            demo.Test02();
+            demo.Test03();
+
+            stopWatch.Stop();
+
+            return $"Chạy hết {stopWatch.ElapsedMilliseconds}ms";
+        }
+
+        public async Task<string> DemoAsync()
+        {
+            var demo = new Demo();
+            var stopWatch = new Stopwatch();
+
+            stopWatch.Start();
+
+            var a = demo.Test01Async();
+            var b = demo.Test02Async();
+            var c = demo.Test03Async();
+
+            await a; await b; await c;
+
+            stopWatch.Stop();
+
+            return $"Chạy hết {stopWatch.ElapsedMilliseconds}ms";
+        }
+        #endregion
+
+        public IActionResult Upload()
+        {
+            return View();
+        }
+
+        #region Upload Single File
+        public IActionResult SingleFile(IFormFile MyFile)
+        {
+            if (MyFile != null)
+            {
+                var fileName = $"{DateTime.Now.Year}{DateTime.Now.Month}{DateTime.Now.Day}_{MyFile.FileName}";
+
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "Data", fileName);
+
+                using (var file = new FileStream(path, FileMode.Create))
+                {
+                    MyFile.CopyTo(file);
+                    ViewBag.ThongBao = "Upload thành công";
+                }    
+            }
+            else
+                ViewBag.ThongBao = "Upload thất bại";
+            return View("Upload");
+        }
+        #endregion
+
+        #region Upload Multiple File
+        public IActionResult MultipleFile(List<IFormFile> MyFile)
+        {
+            if (MyFile != null)
+            {
+                foreach (var myFile in MyFile)
+                {
+                    var fileName = $"{DateTime.Now.Year}{DateTime.Now.Month}{DateTime.Now.Day}_{myFile.FileName}";
+
+                    var path = Path.Combine(Directory.GetCurrentDirectory(), "Data", fileName);
+
+                    using (var file = new FileStream(path, FileMode.Create))
+                    {
+                        myFile.CopyTo(file);
+                    }
+                }
+                ViewBag.ThongBao = "Upload thành công";
+            }
+            else
+                ViewBag.ThongBao = "Upload thất bại";
+
+            return View("Upload");
+        }
+        #endregion
+    }
+}
---
+++ Day08WebApplication1/Day08WebApplication1/Models/Demo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Day08WebApplication1.Models
+{
+    public class Demo
+    {
+        #region Không Async
+        public string Test01()
+        {
+            Thread.Sleep(2000);
+            return $"Lucky number: {new Random().Next()}";
+        }
+
+        public int Test02()
+        {
+            Thread.Sleep(5000);
+            return new Random().Next(100, 1000);
+        }
+
+        public void Test03()
+        {
+            Thread.Sleep(3000);
+        }
+        #endregion
+
+        #region Có Async
+        public async Task<string> Test01Async()
+        {
+            await Task.Delay(2000);
+            return $"Lucky number: {new Random().Next()}";
+        }
+
+        public async Task<int> Test02Async()
+        {
+            await Task.Delay(5000);
+            return new Random().Next(100, 1000);
+        }
+
+        public async Task Test03Async()
+        {
+            await Task.Delay(3000);
+        }
+        #endregion
+
+    }
+}
